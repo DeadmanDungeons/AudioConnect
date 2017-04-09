5559227c36fe156963166eb799af3e1e59011126
Jon <jon@jonmcpherson.com>
2017-04-08 20:02:31 -0400

    Updates VerifyCommand to handle new single step verification process with server

--- src/com/deadmandungeons/audioconnect/command/VerifyCommand.java
+++ src/com/deadmandungeons/audioconnect/command/VerifyCommand.java
@@ -5,10 +5,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.UUID;
-import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 
 import org.bukkit.Bukkit;
@@ -18,43 +20,59 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerListPingEvent;
-import org.bukkit.scheduler.BukkitTask;
 
 import com.deadmandungeons.audioconnect.AudioConnect;
 import com.deadmandungeons.audioconnect.AudioConnectConfig;
 import com.deadmandungeons.audioconnect.messages.AudioConnectUtils;
 import com.deadmandungeons.connect.commons.ConnectUtils;
-import com.deadmandungeons.deadmanplugin.DeadmanUtils;
+import com.deadmandungeons.deadmanplugin.command.ArgumentInfo;
+import com.deadmandungeons.deadmanplugin.command.ArgumentInfo.ArgType;
 import com.deadmandungeons.deadmanplugin.command.Arguments;
 import com.deadmandungeons.deadmanplugin.command.Command;
 import com.deadmandungeons.deadmanplugin.command.CommandInfo;
+import com.deadmandungeons.deadmanplugin.command.SubCommandInfo;
 import com.google.common.io.BaseEncoding;
+import com.google.common.net.HttpHeaders;
 
 //@formatter:off
 @CommandInfo(
 	name = "Verify",
 	permissions = {"audioconnect.admin.verify"},
-	description = "Initiate ownership verification for this server at <connection.endpoint.host>"
+	subCommands = {
+		@SubCommandInfo(
+			arguments = {
+				@ArgumentInfo(argName = "address", argType = ArgType.VARIABLE)
+			},
+			description = "verify the public address of this server with your account at <connection.endpoint.host>"
+		)
+	}
 )//@formatter:on
 public class VerifyCommand implements Command, Listener {
 	
-	private static final String SERVER_VERIFY_PATH = "/admin/servers/%s/verify";
-	private static final long SERVER_VERIFY_EXPIRE_MILLIS = TimeUnit.MINUTES.toMillis(10);
 	private static final BaseEncoding CREDENTIALS_ENCODING = BaseEncoding.base64().omitPadding();
 	
 	private final AudioConnect plugin = AudioConnect.getInstance();
 	
 	private boolean registeredListener;
-	private String encodedVerifyCode;
-	private BukkitTask verifyExpireTask;
+	private VerifyTask activeVerifyTask;
+	
 	
 	@Override
 	public boolean execute(CommandSender sender, Arguments args) {
 		Arguments.validateType(args, getClass());
 		
+		if (activeVerifyTask != null) {
+			plugin.getMessenger().sendErrorMessage(sender, "failed.verify-in-progress");
+			return false;
+		}
 		AudioConnectConfig config = plugin.getConfiguration();
 		if (!config.validate()) {
 			plugin.getMessenger().sendErrorMessage(sender, "failed.verify-invalid-config");
+			return false;
+		}
+		String address = (String) args.getArgs()[0];
+		if (!validateAddress(address)) {
+			plugin.getMessenger().sendErrorMessage(sender, "failed.verify-invalid-address");
 			return false;
 		}
 		
@@ -63,49 +81,59 @@ public class VerifyCommand implements Command, Listener {
 			registeredListener = true;
 		}
 		
-		Bukkit.getScheduler().runTaskAsynchronously(plugin, new VerifyTask(sender, config));
+		plugin.getMessenger().sendMessage(sender, "misc.verify-request", config.getConnectionHost());
+		
+		activeVerifyTask = new VerifyTask(sender, config, address);
+		Bukkit.getScheduler().runTaskAsynchronously(plugin, activeVerifyTask);
 		return true;
 	}
 	
 	
 	@EventHandler
 	private void onServerPing(ServerListPingEvent event) {
-		if (encodedVerifyCode != null) {
+		String encodedVerifyCode;
+		if (activeVerifyTask != null && (encodedVerifyCode = activeVerifyTask.encodedVerifyCode) != null) {
 			event.setMotd(encodedVerifyCode + ChatColor.RESET + event.getMotd());
 		}
 	}
 	
 	
-	private void setVerifyCode(String verifyCode, long expireDelayMillis) {
-		if (verifyExpireTask != null) {
-			verifyExpireTask.cancel();
+	private static boolean validateAddress(String address) {
+		try {
+			URI uri = new URI("mc://" + address);
+			String host = uri.getHost();
+			int port = uri.getPort();
+			return host != null && port <= 65535 && address.equals(host + (port > 0 ? ":" + port : ""));
+		} catch (URISyntaxException e) {
+			return false;
 		}
-		
-		// Encode the verify code using the Minecraft formatting codes (ChatColor)
-		// so that it is invisible when injected into the MOTD during server ping
-		encodedVerifyCode = AudioConnectUtils.encodeFormattingCodes(verifyCode);
-		verifyExpireTask = Bukkit.getScheduler().runTaskLater(plugin, new Runnable() {
-			
-			@Override
-			public void run() {
-				encodedVerifyCode = null;
-				verifyExpireTask = null;
-			}
-		}, expireDelayMillis / 50); // 50 milliseconds per tick
 	}
 	
 	
 	private class VerifyTask implements Runnable {
 		
+		private static final String VERIFY_PATH = "/admin/servers/%s/verify";
+		private static final long VERIFY_EXPIRE_MILLIS = 30000; // Verification should take at most 30 seconds
+		private static final int VERIFY_DELAY_MILLIS = 1000; // Retry requests should be delayed at least 1 second
+		
+		private static final int HTTP_UNPROCESSABLE_ENTITY = 422;
 		private static final String USER_AGENT = "AudioConnect";
+		private static final String CHARSET = "UTF-8";
 		
 		private final UUID playerId;
+		private final String address;
+		private final long startTime;
 		
 		private final String credentials;
 		private final URL verifyUrl;
 		
-		private VerifyTask(CommandSender sender, AudioConnectConfig config) {
+		private volatile String verifyCode;
+		private volatile String encodedVerifyCode;
+		
+		private VerifyTask(CommandSender sender, AudioConnectConfig config, String address) {
 			this.playerId = (sender instanceof Player ? ((Player) sender).getUniqueId() : null);
+			this.address = address;
+			this.startTime = System.currentTimeMillis();
 			
 			String rawCredentials = config.getConnectionUserId() + ":" + config.getConnectionUserPassword();
 			credentials = CREDENTIALS_ENCODING.encode(rawCredentials.getBytes(StandardCharsets.UTF_8));
@@ -114,7 +142,7 @@ public class VerifyCommand implements Command, Listener {
 				URL baseUrl = config.getConnectionWebappUrl();
 				UUID serverId = config.getConnectionServerId();
 				
-				verifyUrl = new URL(baseUrl + String.format(SERVER_VERIFY_PATH, ConnectUtils.encodeUuidBase64(serverId)));
+				verifyUrl = new URL(baseUrl + String.format(VERIFY_PATH, ConnectUtils.encodeUuidBase64(serverId)));
 			} catch (MalformedURLException e) {
 				throw new IllegalStateException("Invalid server verify URL", e);
 			}
@@ -123,83 +151,117 @@ public class VerifyCommand implements Command, Listener {
 		@Override
 		public void run() {
 			try {
-				HttpURLConnection connection = (HttpURLConnection) verifyUrl.openConnection();
+				long requestTime = System.currentTimeMillis();
 				
+				String requestParams = "address=" + URLEncoder.encode(address, CHARSET);
+				if (verifyCode != null) {
+					requestParams += "&code=" + URLEncoder.encode(verifyCode, CHARSET);
+					
+					if ((requestTime - startTime) / 1000 % 3 == 0) {
+						sendMessageSync("misc.verify-check");
+					}
+				}
+				
+				byte[] data = requestParams.getBytes(CHARSET);
+				
+				HttpURLConnection connection = (HttpURLConnection) verifyUrl.openConnection();
 				connection.setRequestMethod("POST");
-				connection.setRequestProperty("User-Agent", USER_AGENT);
-				connection.setRequestProperty("Authorization", "Basic " + credentials);
+				connection.setRequestProperty(HttpHeaders.USER_AGENT, USER_AGENT);
+				connection.setRequestProperty(HttpHeaders.AUTHORIZATION, "Basic " + credentials);
+				connection.setRequestProperty(HttpHeaders.CONTENT_TYPE, "application/x-www-form-urlencoded");
+				connection.setRequestProperty(HttpHeaders.CONTENT_LENGTH, String.valueOf(data.length));
 				connection.setUseCaches(false);
 				connection.setDoOutput(true);
 				
-				int responseCode = connection.getResponseCode();
-				if (responseCode == HttpURLConnection.HTTP_NOT_FOUND) {
-					sendErrorMessageSync("failed.verify-response-404");
-					return;
-				} else if (responseCode == HttpURLConnection.HTTP_UNAUTHORIZED) {
-					sendErrorMessageSync("failed.verify-response-401");
-					return;
-				} else if (responseCode != HttpURLConnection.HTTP_OK) {
-					sendErrorMessageSync("failed.verify-response", responseCode);
-					return;
-				}
+				connection.getOutputStream().write(data);
 				
-				String response;
-				try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-					// Read only the first line (server should only respond with an error message or the verify code)
-					response = in.readLine();
-				}
-				
-				long expireTimeDefault = System.currentTimeMillis() + SERVER_VERIFY_EXPIRE_MILLIS;
-				final long expireTime = connection.getHeaderFieldDate("Expires", expireTimeDefault);
-				
-				final String verifyCode = response;
-				
-				// Set verifyCode and message player on main thread
-				Bukkit.getScheduler().runTask(plugin, new Runnable() {
-					
-					@Override
-					public void run() {
-						long expireDelay = expireTime - System.currentTimeMillis();
-						
-						setVerifyCode(verifyCode, expireDelay);
-						
-						CommandSender sender = getSender();
-						String expireDelayFormatted = DeadmanUtils.formatDuration(expireDelay);
-						if (sender instanceof Player) {
-							plugin.getMessenger().sendMessage(sender, "misc.top-bar");
-							plugin.getMessenger().sendImportantMessage(sender, "misc.verify-details", verifyUrl, expireDelayFormatted);
-							plugin.getMessenger().sendMessage(sender, "misc.bottom-bar");
-						} else {
-							plugin.getMessenger().sendImportantMessage(sender, "misc.verify-details", verifyUrl, expireDelayFormatted);
-						}
+				final int responseCode = connection.getResponseCode();
+				if (responseCode == HttpURLConnection.HTTP_ACCEPTED) {
+					try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+						// Read only the first line (server should only respond with the verify code in response body)
+						verifyCode = in.readLine();
+						// Encode the verify code using the Minecraft formatting codes (ChatColor)
+						// so that it is invisible when injected into the MOTD during server ping
+						encodedVerifyCode = AudioConnectUtils.encodeFormattingCodes(verifyCode);
 					}
-				});
+					
+					long expireTimeDefault = startTime + VERIFY_EXPIRE_MILLIS;
+					long expireTime = connection.getHeaderFieldDate(HttpHeaders.EXPIRES, expireTimeDefault);
+					if (expireTime > expireTimeDefault) {
+						expireTime = expireTimeDefault;
+					}
+					
+					long currentTime = System.currentTimeMillis();
+					if (currentTime < expireTime) {
+						long retryTimeDefault = requestTime + VERIFY_DELAY_MILLIS;
+						long retryTime = connection.getHeaderFieldDate(HttpHeaders.RETRY_AFTER, retryTimeDefault);
+						if (retryTime < retryTimeDefault) {
+							retryTime = retryTimeDefault;
+						}
+						
+						long retryDelay = retryTime - currentTime;
+						Bukkit.getScheduler().runTaskLaterAsynchronously(plugin, this, retryDelay / 50);
+					} else {
+						finish("failed.verify-invalid");
+					}
+				} else if (responseCode == HttpURLConnection.HTTP_OK) {
+					finish("succeeded.address-verified", address, verifyUrl.getHost());
+				} else if (responseCode == HTTP_UNPROCESSABLE_ENTITY) {
+					finish("failed.verify-invalid");
+				} else if (responseCode == HttpURLConnection.HTTP_NOT_FOUND) {
+					finish("failed.verify-response-404");
+				} else if (responseCode == HttpURLConnection.HTTP_UNAUTHORIZED) {
+					finish("failed.verify-response-401");
+				} else if (responseCode == HttpURLConnection.HTTP_CONFLICT) {
+					finish("failed.verify-response-409");
+				} else {
+					finish("failed.verify-response", responseCode);
+				}
 			} catch (IOException e) {
-				sendErrorMessageSync("failed.verify-error");
+				finish("failed.verify-error");
 				String errorMsg = "Failed to verify server at " + verifyUrl;
 				plugin.getLogger().log(Level.SEVERE, errorMsg, e);
 			}
 		}
 		
-		private void sendErrorMessageSync(final String path, final Object... vars) {
-			// Handle sending messages to the player on the main thread
+		private void finish(final String msgPath, final Object... msgVars) {
+			verifyCode = encodedVerifyCode = null;
+			
+			// clear activeVerifyTask and send messages to the command sender on the main thread
 			Bukkit.getScheduler().runTask(plugin, new Runnable() {
 				
 				@Override
 				public void run() {
-					plugin.getMessenger().sendErrorMessage(getSender(), path, vars);
+					activeVerifyTask = null;
+					
+					sendMessage(msgPath, msgVars);
 				}
 			});
 		}
 		
-		private CommandSender getSender() {
+		private void sendMessageSync(final String msgPath, final Object... msgVars) {
+			Bukkit.getScheduler().runTask(plugin, new Runnable() {
+				
+				@Override
+				public void run() {
+					sendMessage(msgPath, msgVars);
+				}
+			});
+		}
+		
+		private void sendMessage(String msgPath, Object... msgVars) {
+			CommandSender sender = Bukkit.getConsoleSender();
 			if (playerId != null) {
 				Player player = Bukkit.getPlayer(playerId);
 				if (player != null && player.isOnline()) {
-					return player;
+					sender = player;
 				}
 			}
-			return Bukkit.getConsoleSender();
+			if (msgPath.startsWith("failed")) {
+				plugin.getMessenger().sendErrorMessage(sender, msgPath, msgVars);
+			} else {
+				plugin.getMessenger().sendMessage(sender, msgPath, msgVars);
+			}
 		}
 		
 	}
